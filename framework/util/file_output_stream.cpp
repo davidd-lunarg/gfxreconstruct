841eb0ad9e40d76e8662ccc8e019f87dcc1d132a
David Donaldson <daviddonaldson@lunarg.com>
2021-05-07 09:18:28 -0700

    WIP: buffered file
    Change-Id: Ic38aee6e1ee2d96ed88e4ebd31ab81053b799472

--- framework/util/file_output_stream.cpp
+++ framework/util/file_output_stream.cpp
@@ -26,42 +26,120 @@
 #include "util/logging.h"
 #include "util/platform.h"
 
+#include <shared_mutex>
+
 GFXRECON_BEGIN_NAMESPACE(gfxrecon)
 GFXRECON_BEGIN_NAMESPACE(util)
 
+const size_t kBufferCount = 4;
+
 FileOutputStream::FileOutputStream(const std::string& filename, size_t buffer_size, bool append) :
+    write_thread_done_(false), buffer_size_(64 * 1024 * 1024), current_buffer_(nullptr), current_buffer_index_(0),
     file_(nullptr), own_file_(true)
 {
     const char* mode   = append ? "ab" : "wb";
     int32_t     result = platform::FileOpen(&file_, filename.c_str(), mode);
 
+    for (size_t i = 0; i < kBufferCount; ++i)
+    {
+        FileBuffer* new_buffer = new FileBuffer();
+        new_buffer->data.resize(buffer_size_);
+        new_buffer->position = 0;
+        buffers_[i] = new_buffer;
+    }
+    current_buffer_ = buffers_[current_buffer_index_];
+
     if (file_ != nullptr)
     {
-        result = platform::SetFileBufferSize(file_, buffer_size);
+        /*result = platform::SetFileBufferSize(file_, buffer_size_);
         if (result != 0)
         {
             GFXRECON_LOG_WARNING("Failed to set file buffer size. File writing performance may be affected.");
-        }
+        }*/
     }
     else
     {
         GFXRECON_LOG_ERROR("fopen(%s, %s) failed (errno = %d)", filename.c_str(), mode, result);
     }
+
+    write_thread_ = std::thread(&FileOutputStream::FileWriteWorker, this);
 }
 
-FileOutputStream::FileOutputStream(FILE* file, bool owned) : file_(file), own_file_(owned) {}
+FileOutputStream::FileOutputStream(FILE* file, bool owned) : file_(file), own_file_(owned), buffer_size_(0)
+{
+    // TODO support this constructor with buffers or remove it
+    assert(false);
+}
 
 FileOutputStream::~FileOutputStream()
 {
+    write_thread_done_ = true;
+    write_thread_.join();
     if ((file_ != nullptr) && own_file_)
     {
         platform::FileClose(file_);
+    }
+    for (FileBuffer* buffer : buffers_)
+    {
+        delete buffer;
     }
 }
 
 size_t FileOutputStream::Write(const void* data, size_t len)
 {
-    return platform::FileWrite(data, 1, len, file_);
+    // TODO clean this up
+    if (len > buffer_size_)
+    {
+        shared_mutex_.lock_shared();
+        need_buffer_swap_.store(true);
+        shared_mutex_.unlock_shared();
+
+        // Wait until buffer swap has completed.
+        while (need_buffer_swap_.load())
+        {}
+
+        shared_mutex_.lock();
+        platform::FileWrite(data, 1, len, file_);
+        shared_mutex_.unlock();
+
+        GFXRECON_LOG_WARNING("Write data of len %" PRIu64, len);
+        return 0;
+    }
+
+    shared_mutex_.lock_shared();
+
+    // Reserve bytes in the current buffer.
+    FileBuffer* write_buffer = current_buffer_;
+    size_t      write_pos    = write_buffer->position.fetch_add(len);
+
+    // Determine if a buffer swap is needed.
+    while ((write_pos + len) > buffer_size_)
+    {
+        need_buffer_swap_.store(true);
+
+        // Indicate to old buffer not to write the reserved bytes.
+        write_buffer->position.fetch_sub(len);
+
+        // Allow write thread to acquire unique lock to change buffer.
+        shared_mutex_.unlock_shared();
+
+        // Wait until buffer swap has completed.
+        while (need_buffer_swap_.load())
+        {}
+
+        // Try again.
+        shared_mutex_.lock_shared();
+
+        // Get new buffer and reserve bytes.
+        write_buffer = current_buffer_;
+        write_pos    = write_buffer->position.fetch_add(len);
+    }
+
+    util::platform::MemoryCopy(write_buffer->data.data() + write_pos, len, data, len);
+
+    shared_mutex_.unlock_shared();
+
+    return len;
 }
 
 GFXRECON_END_NAMESPACE(util)
--- framework/util/file_output_stream.h
+++ framework/util/file_output_stream.h
@@ -25,10 +25,14 @@
 #define GFXRECON_UTIL_FILE_OUTPUT_STREAM_H
 
 #include "util/defines.h"
+#include "util/logging.h"
 #include "util/output_stream.h"
 #include "util/platform.h"
 
+#include <array>
 #include <cstdio>
+#include <inttypes.h>
+#include <mutex>
 #include <string>
 
 GFXRECON_BEGIN_NAMESPACE(gfxrecon)
@@ -53,6 +57,125 @@ class FileOutputStream : public OutputStream
   private:
     FILE* file_;
     bool  own_file_;
+
+    class SharedMutex
+    {
+      public:
+        SharedMutex() : reader_count_(0), wait_for_writer_(false) {}
+
+        void lock()
+        {
+            writer_mutex.lock();
+            wait_for_writer_.store(true);
+
+            while (reader_count_.load() > 0)
+            {}
+        }
+
+        void unlock()
+        {
+            writer_mutex.unlock();
+            wait_for_writer_.store(false);
+        }
+
+        void lock_shared()
+        {
+            reader_count_.fetch_add(1);
+            if (wait_for_writer_.load())
+            {
+                reader_count_.fetch_sub(1);
+                writer_mutex.lock();
+                reader_count_.fetch_add(1);
+                writer_mutex.unlock();
+            }
+        }
+
+        void unlock_shared() { reader_count_.fetch_sub(1); }
+
+      private:
+        std::mutex         writer_mutex;
+        std::atomic_size_t reader_count_;
+        std::atomic_bool   wait_for_writer_;
+    };
+
+    SharedMutex shared_mutex_;
+
+    struct FileBuffer
+    {
+        std::vector<uint8_t> data;
+        std::atomic_size_t   position;
+    };
+
+    std::atomic_bool need_flush_{ false };
+    std::atomic_bool need_buffer_swap_{ false };
+
+    void FileWriteWorker()
+    {
+        const size_t kWriteInterval = 1024 * 1024;
+        size_t       previous_write = 0;
+
+        while (!write_thread_done_)
+        {
+            // Check if a buffer write or swap needs to happen.
+            size_t write_pos  = current_buffer_->position.load();
+            size_t write_size = write_pos - previous_write;
+            if (write_size > kWriteInterval || need_buffer_swap_.load())
+            {
+                // Acquire unique lock to ensure all pending memory copies to addresses below write_pos are done.
+                shared_mutex_.lock();
+
+                // Get actual bytes now that main threads are waiting.
+                write_pos  = current_buffer_->position.load();
+                write_size = write_pos - previous_write;
+
+                if (need_buffer_swap_.load())
+                {
+                    uint8_t* final_write_ptr = current_buffer_->data.data() + previous_write;
+                    SwapBuffers();
+                    previous_write = 0;
+                    need_buffer_swap_.store(false);
+
+                    // Buffer swap complete, allow main threads to continue;
+                    shared_mutex_.unlock();
+
+                    // Write remaining bytes to completed buffer.
+                    platform::FileWrite(final_write_ptr, 1, write_size, file_);
+                }
+                else
+                {
+                    // For normal operation, the main threads can continue while the prepared data is written to file.
+                    shared_mutex_.unlock();
+
+                    platform::FileWrite(current_buffer_->data.data() + previous_write, 1, write_size, file_);
+                    previous_write = write_pos;
+                }
+            }
+            std::this_thread::yield();
+        }
+
+        // write remainder of buffer to disk
+        platform::FileWrite(
+            current_buffer_->data.data() + previous_write, 1, current_buffer_->position.load() - previous_write, file_);
+    }
+
+    void SwapBuffers()
+    {
+        ++current_buffer_index_;
+        current_buffer_index_ = current_buffer_index_ % kBufferCount;
+        current_buffer_       = buffers_[current_buffer_index_];
+        current_buffer_->position.store(0);
+    }
+
+    bool write_thread_done_;
+
+    static const size_t kBufferCount = 2;
+
+    const size_t                          buffer_size_;
+    std::array<FileBuffer*, kBufferCount> buffers_;
+    FileBuffer*                           current_buffer_;
+    size_t                                current_buffer_index_;
+
+    std::thread write_thread_;
 };
 
 GFXRECON_END_NAMESPACE(util)
